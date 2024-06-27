76dae07e826ce5b96157d8f96591e3fb85280ff6
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2024-06-27 17:00:26 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,18 +1,16 @@
 import sys
 n, m = map(int, sys.stdin.readline().split())
 answer = []
-idx = 0 #answer에 요소가 추가될 위치를 가리키는 변수
+idx = 1 #시작 범위가 될 변수
 
 def back(answer, m, n, idx):
     if len(answer) == m: #받은 배열이 m길이가 되면
         print(*answer) #출력
         return
     else:
-        for i in range(1, n+1): #증가시키면서
-            if (idx == 0) or (answer[idx-1] < i) : #answer이 비어있거나 바로 앞의 수보다 지금 추가될 수가 더 클 때만
-                answer.append(i) #배열에 추가
-                idx += 1 #위치를 +1
-                back(answer, m, n, idx) #추가된 배열로 다시 호출
-                del answer[-1] #위의 if문에서 리턴을 타고 오면 여기로 옴, 마지막 원소를 지움
-                idx -= 1 #위치를 -1
+        for i in range(idx, n+1): #증가시키면서
+            idx = i + 1 #idx부터 시작할 수 있도록 업데이트
+            answer.append(i) #배열에 추가
+            back(answer, m, n, idx) #추가된 배열로 다시 호출
+            del answer[-1] #위의 if문에서 리턴을 타고 오면 여기로 옴, 마지막 원소를 지움
 back(answer, m, n, idx)
