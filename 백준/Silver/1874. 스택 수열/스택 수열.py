7f69e0848cec87414e6adfe27fdc68f57fc861ce
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-03-13 15:00:41 +0900

    [Silver II] Title: 스택 수열, Time: 220 ms, Memory: 38640 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -12,22 +12,19 @@ push_pop = []
 stack = deque()
 num = 1
 while 1:
-    if num == n+1:
-        if not stack:
-            break
-        elif stack[0] != answer_list[0]:
+    if stack or num <= n:
+        if stack and stack[0] == answer_list[0]:
+            stack.popleft()
+            answer_list.popleft()
+            push_pop.append("-")
+        elif num <= n:
+            stack.appendleft(num)
+            push_pop.append("+")
+            num += 1
+        elif num > n:
             push_pop = ["NO"]
             break
-    if stack and (stack[0] == answer_list[0]):
-        stack.popleft()
-        answer_list.popleft()
-        push_pop.append("-")
-    elif num <= answer_list[0]:
-        stack.appendleft(num)
-        push_pop.append("+")
-        num += 1
-    elif num > answer_list[0]:
-        push_pop = ["NO"]
+    else:
         break
 
 for push_or_pop in push_pop:
