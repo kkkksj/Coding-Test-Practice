406db8e347c6d7a348011eb87b8b9afcf8719130
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-04-11 21:30:48 +0900

    [level 2] Title: 구명보트, Time: 8.85 ms, Memory: 9.54 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    
+    s, e = 0, len(people)-1
+    while s <= e:
+        if people[s] + people[e] > limit:
+            e -= 1
+        else:
+            s += 1
+            e -= 1
+        answer += 1
+    return answer
