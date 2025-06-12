e4b3cca0257a46a7103030d0b35d41391841297c
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-06-12 15:33:08 +0900

    [Silver V] Title: 배열 합치기, Time: 1296 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -9,10 +9,36 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<int> arr1 (n+m, 0);
-    for(int i=0; i<n+m; i++)
+    vector<int> arr1 (n, 0);
+    vector<int> arr2 (m, 0);
+    vector<int> ans (m+n, 0);
+    for(int i=0; i<n; i++)
         cin >> arr1[i];
-    sort(arr1.begin(), arr1.end());
+    for(int i=0; i<m; i++)
+        cin >> arr2[i];
+    
+    int idx1 = 0, idx2 = 0, i = 0;
+    while(i < n+m){
+        if(idx1 == n){
+            ans[i] = arr2[idx2];
+            idx2++;
+        }
+        else if(idx2 == m){
+            ans[i] = arr1[idx1];
+            idx1++;
+        }
+        else{
+            if(arr1[idx1] <= arr2[idx2]){
+                ans[i] = arr1[idx1];
+                idx1++;
+            }
+            else{
+                ans[i] = arr2[idx2];
+                idx2++;
+            }
+        }
+        i++;
+    }
     for(int i=0; i<n+m; i++)
-        cout << arr1[i] << ' ';
+        cout << ans[i] << ' ';
 }
