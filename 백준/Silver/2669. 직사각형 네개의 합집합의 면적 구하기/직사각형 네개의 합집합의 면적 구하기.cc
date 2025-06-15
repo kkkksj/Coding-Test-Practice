d62f956ebc93fd79a2627268c960da6a97e88ebe
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-06-15 22:33:14 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cc
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.cc
@@ -4,17 +4,6 @@
 #include <cstring>
 using namespace std;
 
-bool Sort_Medal(vector<int> a, vector<int> b){
-    if(a[1] != b[1])
-        return a[1] > b[1];
-    else if(a[2] != b[2])
-        return a[2] > b[2];
-    else if(a[3] != b[3])
-        return a[3] > b[3];
-    else    
-        return a[0] < b[0];
-}
-
 int main() { 
     vector<vector<int>> xy (101, vector<int> (101, 0));
     int sx, sy, ex, ey;
