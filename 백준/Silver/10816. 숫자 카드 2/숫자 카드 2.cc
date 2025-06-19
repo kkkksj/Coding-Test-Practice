3572ba2eedd08e722cedd5b8c880171f3b3a9e14
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-06-19 16:56:28 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 744 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <cstring>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n, m;
+    signed int num;
+    cin >> n;
+
+    map<signed int, int> card;
+    for(int i=0; i<n; i++){
+        cin >> num;
+        card[num]++;
+    }
+    
+    cin >> m;
+    for(int i=0; i<m; i++){
+        cin >> num;
+        if(card.find(num) != card.end())
+            cout << card[num] << ' ';
+        else
+            cout << 0 << ' ';
+    }
+}
