157fcd67e65a02f6abdb38f163aac87fdab5cae1
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-06-27 15:54:52 +0900

    [Silver IV] Title: 카드, Time: 140 ms, Memory: 9796 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+bool Sort(pair<signed long long, int> a, pair<signed long long, int> b){
+    if(a.second != b.second)
+        return a.second > b.second;
+    return a.first < b.first;
+}
+
+int main() {
+    int n;
+    cin >> n; 
+    
+    signed long long num;
+    map<signed long long, int> card;
+    for(int i=0; i<n; i++){
+        cin >> num;
+        card[num]++;
+    }
+
+    vector<pair<signed long long, int>> vec_care (card.begin(), card.end());
+    sort(vec_care.begin(), vec_care.end(), Sort);
+
+    cout << vec_care[0].first << '\n';
+}
