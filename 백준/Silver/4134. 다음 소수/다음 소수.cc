850cd26e926b133b6504b36726431d1e6cb3f406
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-06-28 16:19:14 +0900

    [Silver IV] Title: 다음 소수, Time: 244 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+bool IsPrime(long long num){
+    if(num == 1)
+        return 0;
+    for(long long i=2; i<=sqrt(num); i++)
+        if(num%i == 0)
+            return 0;
+    return 1;
+}
+
+int main() {
+    long long n;
+    int t;
+    cin >> t;
+
+    for(int i=0; i<t; i++){
+        cin >> n;
+        if(n==0)
+            cout << 2 << '\n';
+        else{
+            for(long long num = n; num<num*2; num++){
+                if(IsPrime(num)){
+                    cout << num << '\n';
+                    break;
+                }
+            }
+        }
+    }
+}
