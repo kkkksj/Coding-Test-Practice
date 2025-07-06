32c7a4d2c7cc176d53bea293ea0288f7a23373cc
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-07-06 22:21:57 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void Combi(vector<int> arr, vector<int> &ans, int n, int m){
+    if((int)ans.size() == m){
+        for(int tmp : ans)
+            cout << tmp << ' ';
+        cout << '\n';
+    }
+    else
+        for(int tmp : arr)
+            if(ans.empty() || ans.back() < tmp){
+                ans.push_back(tmp);
+                Combi(arr, ans, n, m);
+                ans.pop_back();
+            }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> arr (n);
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    vector<int> ans;
+    Combi(arr, ans, n, m);
+}
