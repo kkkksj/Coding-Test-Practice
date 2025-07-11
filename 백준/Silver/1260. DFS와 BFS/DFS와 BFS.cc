d6ceebb0f356579fe83554e779c27506556c426f
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-07-11 13:47:24 +0900

    [Silver II] Title: DFS와 BFS, Time: 8 ms, Memory: 4008 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+using namespace std;
+
+void DFS(vector<bool> &visited, map<int, set<int>> &graph, int node){
+    cout << node << ' ';
+    visited[node] = true;
+
+    set<int> connected = graph[node];
+    for(auto n = connected.begin(); n != connected.end(); n++)
+        if(!visited[*n]){
+            visited[*n] = true;
+            DFS(visited, graph, *n);
+        }
+    return ;
+}
+
+void BFS(vector<bool> &visited, map<int, set<int>> &graph, int &node){
+    queue<int> q;
+    q.push(node);
+    visited[node] = true;
+
+    while(!q.empty()){
+        node = q.front();
+        cout << node << ' ';
+        q.pop();
+        set<int> connected = graph[node];
+        for(auto n = connected.begin(); n != connected.end(); n++)
+            if(!visited[*n]){
+                visited[*n] = true;
+                q.push(*n);
+            }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, m, v;
+    cin >> n >> m >> v;
+
+    map<int, set<int>> graph;
+    int a, b;
+    for(int i=0; i<m; i++){
+        cin >> a >> b;
+        graph[a].insert(b);
+        graph[b].insert(a);
+    }
+
+    vector<bool> visited1 (n+1, false);
+    DFS(visited1, graph, v);
+    cout << '\n';
+    vector<bool> visited2 (n+1, false);
+    BFS(visited2, graph, v);
+}
