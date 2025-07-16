240e26db44915db64e0b1428bf01f600021d3aee
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-07-16 21:03:51 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 48 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,23 +1,24 @@
 #include <iostream>
-#include <algorithm>
-#include <map>
+#include <vector>
 #include <queue>
 using namespace std;
 
-void BFS(vector<int> &tree, map<int, queue<int>> &graph, int point, vector<bool> &visited, queue<int> &to_visit){
-    int node;
-    while(!to_visit.empty()){
-        node = to_visit.front();
-        to_visit.pop();
-        while(!graph[node].empty()){
-            int next = graph[node].front();
-            graph[node].pop();
-            if(visited[next] == false){
+void BFS(vector<int>& tree, vector<vector<int>>& graph, vector<bool>& visited) {
+    queue<int> q;
+    q.push(1);
+    visited[1] = true;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                visited[next] = true;
                 tree[next] = node;
-                to_visit.push(next);
+                q.push(next);
             }
         }
-        visited[node] = true;
     }
 }
 
@@ -28,20 +29,19 @@ int main() {
     int n;
     cin >> n;
 
-    map<int, queue<int>> graph;
+    vector<vector<int>> graph(n+1);
     int a, b;
-    for(int i=0; i<n-1; i++){
+    for (int i = 0; i < n-1; i++) {
         cin >> a >> b;
-        graph[a].push(b);
-        graph[b].push(a);
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
 
     vector<int> tree(n+1, 0);
     vector<bool> visited(n+1, false);
-    queue<int> to_visit;
-    to_visit.push(1);
-    BFS(tree, graph, 1, visited, to_visit);
 
-    for(int i=2; i<n+1; i++)
+    BFS(tree, graph, visited);
+
+    for (int i = 2; i <= n; i++)
         cout << tree[i] << '\n';
 }
