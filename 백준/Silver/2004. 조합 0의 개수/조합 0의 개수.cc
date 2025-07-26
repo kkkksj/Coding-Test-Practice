6cb4c9483dc14b8fcab6420e514c5fc191d6a09a
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-07-26 12:46:37 +0900

    [Silver II] Title: 조합 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+long long n, m;
+
+long long howmany5(long long num){
+    long long ret = 0;
+    for(long long i=5; i<=num; i*=5)
+        ret += num/i;
+    return ret;
+}
+
+long long howmany2(long long num){
+    long long ret = 0;
+    for(long long i=2; i<=num; i*=2)
+        ret += num/i;
+    return ret;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+
+    long long five, two;
+    five = howmany5(n) - howmany5(m) - howmany5(n-m);
+    two = howmany2(n) - howmany2(m) - howmany2(n-m);
+
+    cout << min(five, two) << '\n';
+}
