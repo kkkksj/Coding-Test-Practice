973f66094eb3fa9fd90306566ec9de9a8f3b9689
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-09-27 23:28:06 +0900

    [Gold IV] Title: N-Queen, Time: 3820 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+vector<int> col(16);
+int n;
+int ans = 0;
+
+void queen(int x){
+    if(n == x)
+        ans++; 
+    else{
+        for (int i = 0; i < n;i++){
+            col[x] = i; 
+            bool can = true;
+            for (int j = 0; j < x;j++){
+                if(col[x] == col[j] || abs(col[x] - col[j]) == x - j){
+                    can = false; 
+                    break;
+                }
+            }
+            if(can)
+                queen(x + 1);
+        }
+    }
+} 
+ 
+int main(){
+    cin >> n;
+    queen(0);
+    cout << ans;
+} 
