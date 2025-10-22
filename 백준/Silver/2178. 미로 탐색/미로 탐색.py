73dc20780b04aac5381a6d9e2e3b8740708f51fc
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-10-22 13:29:18 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,20 +1,35 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
+
+i_list = [0, 0, -1, 1]
+j_list = [-1, 1, 0, 0]
+def BFS():
+    q = deque()
+    visited = [[False for j in range(m)]for i in range(n)]
+    q.append([0, 0, 1])
+    visited[0][0] = True
+
+    while len(q):
+        now_i, now_j, cnt = q.popleft()
+        if now_i == n-1 and now_j == m-1:
+            return cnt
+
+        for d in range(4):
+            next_i = now_i + i_list[d]
+            next_j = now_j + j_list[d]
+
+            if next_i<0 or next_i>=n or next_j<0 or next_j>=m or visited[next_i][next_j] or grid[next_i][next_j]=='0':
+                continue
+
+            visited[next_i][next_j] = True
+            q.append([next_i, next_j, cnt+1])
+
+    return -1
 
 n, m = map(int, input().split())
-
-maze = []
+grid = []
 for i in range(n):
-    maze.append(list(map(int, list(input()))))
+    grid.append(input())
 
-i_list = [-1, 1, 0, 0]
-j_list = [0, 0, -1, 1]
-q = deque()
-q.append((0, 0))
-while q:
-    row, col = q.popleft()
-    for i, j in zip(i_list, j_list):
-        next_row, next_col = row+i, col+j
-        if 0<=next_row<n and 0<=next_col<m and maze[next_row][next_col] == 1:
-            maze[next_row][next_col] = maze[row][col] + 1
-            q.append((next_row, next_col))
-print(maze[n-1][m-1])
+print(BFS())
