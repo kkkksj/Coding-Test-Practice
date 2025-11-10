8cc509185fd7e3dfc35b7c074edd3b2e692eae37
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-11-10 16:41:18 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 13 ms, Memory: 5,836 KB -BaekjoonHub

---
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<int>> grid;
+vector<int> j_list = { 1, 0, -1, 0 };
+vector<int> i_list = { 0, 1, 0, -1 };
+
+void Write(int n) {
+	int now_i = 0, now_j = 0;
+	int d = 0;
+	for (int i = 1; i < n * n + 1; i++) {
+		grid[now_i][now_j] = i;
+		if (now_i + i_list[d] < 0 || now_i + i_list[d] >= n || now_j + j_list[d] < 0 || now_j + j_list[d] >= n || grid[now_i + i_list[d]][now_j + j_list[d]] != 0)
+			d = (d + 1) % 4;
+		now_i += i_list[d];
+		now_j += j_list[d];
+	}
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin >> T;
+
+	vector<int> d_list = { -2, -1, 1, 2 };
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int n;
+		cin >> n;
+
+		vector<int> cnts(101, 0);
+		int maxnum = 0;
+		for (int i = 0; i < 1000; i++) {
+			int num;
+			cin >> num;
+			cnts[num]++;
+			maxnum = max(maxnum, cnts[num]);
+		}
+		for(int i=100; i>0; i--)
+			if (cnts[i] == maxnum){
+				cout << '#' << n << ' ' << i << '\n';
+				break;
+			}
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
