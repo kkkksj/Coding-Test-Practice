d59e7b26c605dbd370f5664d7d4a77ca79e64941
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-11-11 15:42:18 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 11 ms, Memory: 5,844 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.cpp
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.cpp
@@ -4,62 +4,23 @@
 
 using namespace std;
 int n;
-
-struct Info {
-	int num, idx;
-};
-
-struct CMP1 {
-	bool operator()(Info a, Info b) {
-		if (a.num != b.num)
-			return a.num < b.num;
-		return a.idx < b.idx;
-	}
-};
-struct CMP2 {
-	bool operator()(Info a, Info b) {
-		if (a.num != b.num)
-			return a.num> b.num;
-		return a.idx > b.idx;
-	}
-};
-
-priority_queue<Info, vector<Info>, CMP1> bpq;
-priority_queue<Info, vector<Info>, CMP2> spq;
-vector<int> num_list;
+priority_queue<int> bpq;
+priority_queue<int, vector<int>, greater<int>> spq;
 
 int Find() {
 	int big, small;
-	int big_idx, small_idx;
 	for (int i = 0; i < n; i++) {
-		big = bpq.top().num;
-		big_idx = bpq.top().idx;
+		big = bpq.top();
 		bpq.pop();
-		if (num_list[big_idx] != big) {
-			bpq.push({ num_list[big_idx], big_idx });
-			big = bpq.top().num;
-			big_idx = bpq.top().idx;
-			bpq.pop();
-		}
-		small = spq.top().num;
-		small_idx = spq.top().idx;
+		small = spq.top();
 		spq.pop();
-		if (num_list[small_idx] != small) {
-			spq.push({ num_list[small_idx], small_idx });
-			small = spq.top().num;
-			small_idx = spq.top().idx;
-			spq.pop();
-		}
 
 		if (big - small <= 1)
 			return big - small;
-		bpq.push({ big - 1, big_idx });
-		spq.push({ small + 1, small_idx });
-		
-		num_list[big_idx] = big - 1;
-		num_list[small_idx] = small + 1;
+		bpq.push(big - 1);
+		spq.push(small + 1);
 	}
-	return bpq.top().num - spq.top().num;
+	return bpq.top() - spq.top();
 }
 
 int main(int argc, char** argv)
@@ -71,16 +32,15 @@ int main(int argc, char** argv)
 	for (test_case = 1; test_case <= T; ++test_case) {
 		cin >> n;
 		
-		bpq = priority_queue<Info, vector<Info>, CMP1>();
-		spq = priority_queue<Info, vector<Info>, CMP2>();
+		bpq = priority_queue<int>();
+		spq = priority_queue<int, vector<int>, greater<int>>();
+
 
 		int num;
-		num_list.assign(100, 0);
 		for (int i = 0; i < 100; i++) {
 			cin >> num;
-			bpq.push({num, i});
-			spq.push({num, i});
-			num_list[i] = num;
+			bpq.push(num);
+			spq.push(num);
 		}
 
 		cout << '#' << test_case << ' ' << Find() << '\n';
