1d4098bdce6c22c3957cc99fd64ec4fdbde9801a
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2025-11-14 16:31:03 +0900

    [D2] Title: 지그재그 숫자, Time: 5 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case) {
+		int n;
+		cin >> n;
+
+		int ans = 0;
+		for (int i = 1; i < n + 1; i++) {
+			if (i % 2 == 0)
+				ans -= i;
+			else
+				ans += i;
+		}
+
+		cout << '#' << test_case << ' ' << ans << '\n';
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
