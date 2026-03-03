a39bbc20145f28c728e90951c32112ccefe4eb1d
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2026-03-03 23:11:29 +0900

    [Gold II] Title: 컵라면, Time: 176 ms, Memory: 9792 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+#define MAX 200000
+
+int n;
+vector<int> arr[MAX + 1];
+
+int Search() {
+	int ret = 0;
+	priority_queue<int> pq;
+
+	for (int i = n; i > 0; i--) {
+		for (int a : arr[i])
+			pq.push(a);
+		if (!pq.empty()) {
+			ret += pq.top();
+			pq.pop();
+		}
+	}
+
+	return ret;
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int deadline, cup;
+		cin >> deadline >> cup;
+
+		arr[deadline].push_back(cup);
+	}
+
+	cout << Search() << '\n';
+
+}
