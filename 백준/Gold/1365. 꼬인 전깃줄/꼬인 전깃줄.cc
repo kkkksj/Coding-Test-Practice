2db709f462757a6253b0e07f2d9dfeff7c8ae8ae
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2026-04-11 01:14:17 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 324 ms, Memory: 2480 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int Lower_Bound(vector<int> vt, int num) {
+    int low = 0, high = vt.size() - 1;
+
+    while (low < high) {
+        int mid = (low + high) / 2;
+        if (vt[mid] >= num)
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    return high;
+}
+
+int main(void)
+{
+    int n, num;
+    cin >> n;
+    vector<int> vt;
+    vt.push_back(-1);
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        if (num > vt[vt.size() - 1])
+            vt.push_back(num);
+        else
+            vt[Lower_Bound(vt, num)] = num;
+    }
+    cout << n - vt.size() + 1;
+
+}
