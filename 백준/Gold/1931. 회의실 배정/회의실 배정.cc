8a738d7f814a134b85732cce98fb1de77ad1b549
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2026-04-14 00:01:59 +0900

    [Gold V] Title: 회의실 배정, Time: 84 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -4,29 +4,26 @@
 
 using namespace std;
 
-bool cmp(pair<int, int> a, pair<int, int> b){
-    if(a.second != b.second)
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    if (a.second != b.second)
         return a.second < b.second;
     return a.first < b.first;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
     int n;
     cin >> n;
 
     vector<pair<int, int>> times(n);
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
         cin >> times[i].first >> times[i].second;
-        
+
     sort(times.begin(), times.end(), cmp);
 
     int end_time = times[0].second;
     int ans = 1;
-    for(int i=1; i<n; i++){
-        if(times[i].first >= end_time){
+    for (int i = 1; i < n; i++) {
+        if (times[i].first >= end_time) {
             end_time = times[i].second;
             ans++;
         }
