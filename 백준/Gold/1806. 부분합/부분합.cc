fd08a444b63e8e4393fbb529b68df97bed9867f0
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2026-04-13 23:43:53 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -1,36 +1,44 @@
 #include <iostream>
 #include <vector>
 
+#define min(a, b) a < b ? a : b
+
 using namespace std;
 
+int n, s;
+vector <long long> prefix_sum_arr;
+
+int Find() {
+	int i = 0, j = 1;;
+	int ret = 100000;
+	while (j <= n) {
+		int ttlsum = prefix_sum_arr[j] - prefix_sum_arr[i];
+		if (ttlsum >= s) {
+			ret = min(ret, (j-i));
+			i++;
+		}
+		else
+			j++;
+		if (i == j)
+			j++;
+	}
+	if (ret == 100000)
+		return 0;
+	return ret;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    int n, s;
-    cin >> n >> s;
+	cin >> n >> s;
+	prefix_sum_arr.assign(n + 1, 0);
 
-    vector<int> arr(n, 0);
-    for(int i=0; i<n; i++)
-        cin >> arr[i];
-    
-    int i = 0, j = 0;
-    int psum = arr[0];
-    int ans = n+1;
+	int num;
+	for (int i = 1; i < n + 1; i++) {
+		cin >> prefix_sum_arr[i];
+		prefix_sum_arr[i] += prefix_sum_arr[i - 1];
+	}
 
-    while(i<=j && j<n){
-        if(psum < s){
-            j++;
-            psum += arr[j];
-        }
-        else{
-            ans = min(ans, j-i+1);
-            psum -= arr[i];
-            i++;
-        }
-    }
-
-    if(ans == n+1)
-        ans = 0;
-    cout << ans << '\n';
+	cout << Find() << '\n';
 }
