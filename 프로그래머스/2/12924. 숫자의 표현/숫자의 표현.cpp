747e6f9324388731876e9ca90e1dc3e41027308d
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2026-05-29 00:01:13 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    int s = 1;
+    int e = 1;
+    int sum = 1;
+
+    while (s <= n) {
+        if (sum == n) {
+            answer++;
+            sum -= s;
+            s++;
+        }
+        else if (sum > n) {
+            sum -= s;
+            s++;
+        }
+        else {
+            e++;
+            if (e > n) break;
+            sum += e;
+        }
+    }
+
+    return answer;
+}
