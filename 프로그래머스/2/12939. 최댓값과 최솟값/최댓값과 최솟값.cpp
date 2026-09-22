6a47c92be52bbf046550a0e9071841a1d9ec8437
Kyung Sejin <117196455+kkkksj@users.noreply.github.com>
2026-09-22 22:48:38 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -7,24 +7,26 @@ string solution(string s) {
     string answer = "";
     
     string tmp = "";
-    int maxnum = -21e8;
-    int minum = 21e8;
+    int maxnum = -1e9;
+    int minnum = 1e9;
     for(char c : s){
         if(c == ' '){
             int num = stoi(tmp);
             maxnum = max(num, maxnum);
-            minum = min(num, minum);
+            minnum = min(num, minnum);
             tmp = "";
         }
         else
             tmp += c;
     }
+    // 마지막 한 번 더 
     int num = stoi(tmp);
     maxnum = max(num, maxnum);
-    minum = min(num, minum);
+    minnum = min(num, minnum);
     
-    answer += to_string(minum);
+    answer += to_string(minnum);
     answer += ' ';
     answer += to_string(maxnum);
+    
     return answer;
 }
